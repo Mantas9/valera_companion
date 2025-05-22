21d3e66711b0192bb38bb4c65280b8b6875c1934
Mantas9 <dev.mantasgr@gmail.com>
2025-05-22 19:28:32 +0300

    Added necessary variables

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ connection = obd.OBD(port, baud) # Auto-Connect to OBD
 # CHANNEL 2
 
 # Get OBD response from specified command
-def get_data(command):
+def get_data(command, include_units: bool = False):
     response = connection.query(command)
     
     if not response.is_null():
-        value = response.value.magnitude
+        value = response.value
+        
+        if not include_units:
+            value = value.magnitude
+        
         return value
     
     return None
@@ -24,10 +28,28 @@ get_rpms = obd.commands.RPM # Command
 bound_rpms = 2500 # Switch gear sommand 
 bound_rpms_redline = 4500 # REDLINING (Volvo s60)
 
-# Coolant Temperature - Celsius
+# Speed
+get_speed = obd.commands.SPEED
+
+# Throttle position
+get_throttle_pos = obd.commands.THROTTLE_POS
+
+# Engine load - how hard the engine is working
+get_engine_load = obd.commands.ENGINE_LOAD
+
+# Coolant temperature - Celsius
 get_cool_temp = obd.commands.COOLANT_TEMP
 bound_cool_operating = 70
 bound_cool_overheat = 105
+
+# Oil temperature - Celsius
+get_oil_temp = obd.commands.OIL_TEMP
+
+# Intake air Temperature - Celsius
+get_intake_temp = obd.commands.INTAKE_TEMP
+
+# Fuel level
+get_fuel_level = obd.commands.FUEL_LEVEL1
 
 
 while True:
@@ -38,10 +60,17 @@ while True:
             connection = obd.OBD(port, baud)
         
         # Get responses
-        rpms = get_data(get_rpms)
-        coolant = get_data(get_cool_temp)
+        rpms = get_data(get_rpms, True)
+        speed = get_data(get_speed, True)
+        throttle_pos = get_data(get_throttle_pos, True)
+        engine_load = get_data(get_engine_load, True)
+        coolant_temp = get_data(get_cool_temp, True)
+        oil_temp = get_data(get_oil_temp, True)
+        intake_temp = get_data(get_intake_temp, True)
+        fuel_level = get_data(get_fuel_level, True)
         
-        print(rpms, coolant)
+        
+        print(rpms, speed, throttle_pos, engine_load, coolant_temp, oil_temp, intake_temp, fuel_level, sep="\n")
 
     except Exception as e:
         print(f"Error: {e}")
