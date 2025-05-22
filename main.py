f4e54c8ac6b282663362f5174127969a3b9bd01c
Mantas9 <dev.mantasgr@gmail.com>
2025-05-22 20:32:45 +0300

    Sort code and update pip requirements

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import obd
 import time
 
-# INITIAL OBD CONNECTION
+# ========== OBD CONNECTION BASICS
 port = "/dev/pts/4"
 baud = 9600
-connection = obd.OBD(port, baud) # Auto-Connect to OBD
-
+connection = obd.OBD(port, baud)  # Auto-Connect to OBD
 # REAL LIFE OBD USES BLUETOOTH CHANNEL 2!!!
 
+# ========== OBD COMMANDS
+
 # Get OBD response from specified command
+
+
 def get_data(command, include_units: bool = False):
     response = connection.query(command)
-    
+
     if not response.is_null():
         value = response.value
-        
+
         if not include_units:
             value = value.magnitude
-        
+
         return value
-    
+
     return None
 
-# OBD COMMANDS
+
 # RPMs
-get_rpms = obd.commands.RPM # Command
-bound_rpms = 2500 # Switch gear sommand 
-bound_rpms_redline = 4500 # REDLINING (Volvo s60)
+get_rpms = obd.commands.RPM  # Command
+bound_rpms = 2500  # Switch gear sommand
+bound_rpms_redline = 4500  # REDLINING (Volvo s60)
 
 # Speed
 get_speed = obd.commands.SPEED
-speed_last = -1 # Speed last second
-speed_veryfast = 140 # Kerosene on 140 km/h
-
+speed_last = -1  # Speed last second
+speed_veryfast = 140  # Kerosene on 140 km/h
 
 # Throttle position
 get_throttle_pos = obd.commands.THROTTLE_POS
@@ -53,8 +55,8 @@ get_intake_temp = obd.commands.INTAKE_TEMP
 
 # Fuel level
 get_fuel_level = obd.commands.FUEL_LEVEL
-fuel_low = 15 # Percent
-fuel_remind_interval = 1800 # Seconds, 1800s - 30min
+fuel_low = 15  # Percent
+fuel_remind_interval = 1800  # Seconds, 1800s - 30min
 
 # Air flow rate (MAF)
 get_maf = obd.commands.MAF
@@ -62,14 +64,14 @@ get_maf = obd.commands.MAF
 # Fuel rate
 get_fuel_rate = obd.commands.FUEL_RATE
 
-
+# ========== RUNTIME CODE
 while True:
     try:
         # Check for connection
         if not connection.is_connected():
             print("Reconnecting to OBD...")
             connection = obd.OBD(port, baud)
-        
+
         # Get responses
         rpms = get_data(get_rpms)
         speed = get_data(get_speed)
@@ -79,18 +81,16 @@ while True:
         oil_temp = get_data(get_oil_temp)
         intake_temp = get_data(get_intake_temp)
         fuel_level = get_data(get_fuel_level)
-        
-        
-        
-        
-        print(rpms, speed, throttle_pos, engine_load, coolant_temp, oil_temp, intake_temp, fuel_level, speed_last, sep="\n")
-        
+
+        print(rpms, speed, throttle_pos, engine_load, coolant_temp,
+              oil_temp, intake_temp, fuel_level, speed_last, sep="\n")
+
         # Set speed to be last second speed at the end
         speed_last = float(speed)
 
     except Exception as e:
         print(f"Error: {e}")
         connection = obd.OBD(port, baud)
-        
+
     # Cycle 1s
     time.sleep(1)
